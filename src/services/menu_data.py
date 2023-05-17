3c0623d6186b9a0d02ceaefcea8c54556afd9e6e
stonefullstm <stonefull.stm@gmail.com>
2023-05-17 08:32:22 -0300

    fix: Ajustes no método create_dishes da classe MenuData

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -7,18 +7,20 @@ from models.ingredient import Ingredient
 class MenuData:
     def __init__(self, source_path: str) -> None:
         self.dishes = set()
-        self.dishes_df = pd.read_csv(source_path).itertuples(index=False)
+        self.dishes_df = pd.read_csv(source_path)
         self.create_dishes()
 
     def create_dishes(self):
-        previous_dish = None
-        dish = None
-        for dish_name, price, ingredient, amount in self.dishes_df:
-            if previous_dish is None:
-                previous_dish = dish_name
+        previous_dish = self.dishes_df["dish"].loc[0]
+        dish = Dish(
+            self.dishes_df["dish"].loc[0],
+            float(self.dishes_df["price"].loc[0])
+            )
+        for dish_name, price, ingredient, amount in self.dishes_df.itertuples(
+                index=False):
             if dish_name != previous_dish:
                 self.dishes.add(dish)
                 previous_dish = dish_name
-            dish = Dish(dish_name, float(price))
+                dish = Dish(dish_name, float(price))
             dish.add_ingredient_dependency(Ingredient(ingredient), amount)
         self.dishes.add(dish)
